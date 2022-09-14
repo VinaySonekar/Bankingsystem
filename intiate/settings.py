e82a6519eed5cbc91b92ad59b2ae3c729d2d8db3
VinsoBlog <vinayso712@gmail.com>
2022-09-14 08:15:38 +0530

    commit

--- intiate/settings.py
+++ intiate/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = '2(u$87!a(-j##^#o0ufl(fudp+4!!nmu-fag1)d1))*r3za7h@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-#ALLOWED_HOSTS = ['https://sparks-bankingg.herokuapp.com/','localhost','127.0.0.1']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://sparks-bankingg.herokuapp.com/','localhost','127.0.0.1']
+
 
 # Application definition
 
